0bf5b6d8641235d9132565ed83922745d2768053
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-08-14 01:23:39 +0600

    chart

--- src/components/ReadTimeChart.jsx
+++ src/components/ReadTimeChart.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { AuthContext } from "./AuthProvider";
 import LoadingState from './LoadingState';
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const ReadTimeChart = () => {
     const [chartData, setChartData] = useState(null);
@@ -35,39 +32,26 @@ const ReadTimeChart = () => {
                 });
 
                 // Prepare data for the chart
-                const titles = [];
-                const readTimes = [];
-                const leftTimes = [];
+                const data = [];
                 let totalRead = 0;
                 let totalLeft = 0;
 
                 blogs.forEach(blog => {
-                    titles.push(blog.title.substring(0, 20));
                     const readTime = readTimeMap.get(blog._id) || 0;
-                    readTimes.push(readTime);
                     const leftTime = parseFloat(blog.time_to_read) - readTime;
-                    leftTimes.push(leftTime);
+
+                    data.push({
+                        name: blog.title.substring(0, 5),
+                        fullTitle: blog.title, // Store the full title for tooltip
+                        readTime: readTime,
+                        leftTime: leftTime,
+                    });
 
                     totalRead += readTime;
                     totalLeft += leftTime;
                 });
 
-                setChartData({
-                    labels: titles,
-                    datasets: [
-                        {
-                            label: 'Read Time',
-                            data: readTimes,
-                            backgroundColor: 'rgba(75, 192, 192, 0.6)',
-                        },
-                        {
-                            label: 'Left Time',
-                            data: leftTimes,
-                            backgroundColor: 'rgba(255, 99, 132, 0.6)',
-                        },
-                    ],
-                });
-
+                setChartData(data);
                 setTotalReadTime(totalRead);
                 setTotalLeftTime(totalLeft);
             } catch (error) {
@@ -80,37 +64,64 @@ const ReadTimeChart = () => {
         }
     }, [user]);
 
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            const item = payload[0].payload;
+            return (
+                <div className="custom-tooltip" style={{ backgroundColor: '#333', color: '#fff', padding: '10px', borderRadius: '5px' }}>
+                    <p className="label">{`${item.fullTitle}`}</p>
+                    <p className="intro">{`Read Time: ${item.readTime.toFixed(0)} Minutes`}</p>
+                    <p className="desc">{`Left Time: ${item.leftTime.toFixed(0)} Minutes`}</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     return (
-        <div className='my-12 max-w-screen-lg mx-auto p-5 min-h-[calc(100vh-150px)] '>
+        <div className='my-12 max-w-screen-lg bg-black mx-auto p-5 min-h-[calc(100vh-150px)]'>
+
+            <h1 className="md:text-3xl text-white text-xl  font-cinzel font-semibold text-center mb-6">Comprehensive Read Time vs. Left Time Overview</h1>
+            <div className='pb-6 flex gap-6 justify-center'>
+                <div className='flex gap-2 items-center'>
+                    <div className='bg-[#8884d8] h-4 w-4'></div>
+                    <p className='text-white'>Total Read Time</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <div className='bg-[#82ca9d] h-4 w-4'></div>
+                    <p className='text-white'>Total Left Time</p>
+                </div>
+            </div>
             {chartData ? (
                 <>
-                    <Bar
-                        data={chartData}
-                        options={{
-                            responsive: true,
-                            plugins: {
-                                legend: {
-                                    position: 'top',
-                                },
-                                title: {
-                                    display: true,
-                                    text: 'Read Time vs Left Time',
-                                },
-                            },
-                        }}
-                    />
-                    <div className='p-6 md:flex gap-6'>
+                    <ResponsiveContainer width="100%" height={400}>
+                        <BarChart
+                            data={chartData}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" stroke="#fff" />
+                            <XAxis dataKey="name" stroke="#fff" />
+                            <YAxis stroke="#fff" />
+                            <Tooltip content={<CustomTooltip />} />
+                            <Legend wrapperStyle={{ color: '#fff' }} />
+                            <Bar dataKey="readTime" barSize={20} fill="#8884d8" />
+                            <Bar dataKey="leftTime" barSize={20} fill="#82ca9d" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                    <div className='p-6 md:flex gap-6 justify-center'>
                         <div className='flex gap-2 items-center'>
-                            <div className='bg-[#93D9D9]  h-4 w-4'>
-
-                            </div>
-                            <p>Total Read Time: {totalReadTime.toFixed(0)} Minutes</p>
+                            <div className='bg-[#8884d8] h-4 w-4'></div>
+                            <p className='text-white'>Total Read Time: {totalReadTime.toFixed(0)} Minutes</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <div className='bg-[#FFA1B5]  h-4 w-4'>
-
-                            </div>
-                            <p>Total Left Time: {totalLeftTime.toFixed(0)} Minutes</p>
+                            <div className='bg-[#82ca9d] h-4 w-4'></div>
+                            <p className='text-white'>Total Left Time: {totalLeftTime.toFixed(0)} Minutes</p>
                         </div>
                     </div>
                 </>
